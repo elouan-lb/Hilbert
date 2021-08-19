e00c659ab9cf6179dae0af0db6b7aa109934cc11
Elouan Le Bars <elouan@elouan-lebars.fr>
2021-08-19 16:02:27 +0200

    [UPD] Disable cache storage

--- src/store/index.js
+++ src/store/index.js
@@ -40,12 +40,12 @@ export default createStore({
   mutations: {
     initialiseStore(state) {
       // Check if the ID exists
-      if (localStorage.getItem("store")) {
-        // Replace the state object with the stored item
-        this.replaceState(
-          Object.assign(state, JSON.parse(localStorage.getItem("store")))
-        );
-      }
+      // if (localStorage.getItem("store")) {
+      //   // Replace the state object with the stored item
+      //   this.replaceState(
+      //     Object.assign(state, JSON.parse(localStorage.getItem("store")))
+      //   );
+      // }
     },
     sendParametersValueWithOSC(state) {
       this.state.parameters.forEach((param) => {
